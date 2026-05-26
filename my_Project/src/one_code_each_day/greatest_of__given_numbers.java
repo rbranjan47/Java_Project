219ffcce75bea41e9c4b06108f35621ea0c6974a
Rabi Ranjan Kumar <rbranjan47@gmail.com>
2026-05-27 01:32:12 +0530

    count_duplicates

--- my_Project/src/one_code_each_day/greatest_of__given_numbers.java
+++ my_Project/src/one_code_each_day/greatest_of__given_numbers.java
@@ -6,6 +6,8 @@ public class greatest_of__given_numbers {
     public static int check_greatest_number(int[] num_list) {
         int greatest_num = num_list[0];
         for (int i = 1; i < num_list.length; i++){
+            // Here I've stored the first element of array/list as maximum value and then comparing with the remaining values
+            // If I am getting maximum value from the 1st position, then I am storing it as maximum value and doing again and again
             if(num_list[i] > greatest_num){
                 greatest_num = num_list[i];
             }
@@ -14,7 +16,7 @@ public class greatest_of__given_numbers {
     }
 
     public static void main(String[] args) {
-        int[] values =  {12, 15, 20, 25, 100, 30, 40, 200};
+        int[] values =  {12, 15, 20, 25, 100, 30, 40, 200, 10};
 
         System.out.println(check_greatest_number(values));
     }
