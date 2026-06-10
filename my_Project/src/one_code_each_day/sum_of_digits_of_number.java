9e927360b3e3090d23085c41193c5cdb6c362b2d
Rabi Ranjan Kumar <rbranjan47@gmail.com>
2026-06-11 00:56:23 +0530

    sum_of_digits

---
+++ my_Project/src/one_code_each_day/sum_of_digits_of_number.java
@@ -0,0 +1,31 @@
+package one_code_each_day;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class sum_of_digits_of_number {
+
+    public static void sum_of_digits_of_given_number(String num) throws Exception {
+        String[] num_array = num.split("");
+        Double sum = 0.00;
+        if (Double.parseDouble(num) < 0) {
+            throw new Exception("Enter valid number");
+        }
+        // Can sum also decimal value, summing only digits
+        for (int i = 0; i < num_array.length; i++) {
+            if (Pattern.compile("\\d").matcher(num_array[i]).find()) {
+                sum = sum + Double.parseDouble(num_array[i]);
+            }
+        }
+        System.out.println(num + " number sum: " + sum);
+    }
+
+    public static void main(String[] args) throws Exception {
+        // In O(n)
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        String value = scn.nextLine();
+
+        sum_of_digits_of_given_number(value);
+    }
+}
